9fa8824dbf56418bf56189e9720b78c92d2de20f
merlinmac <merlin@graalonline.com>
2023-02-21 15:03:44 +0100

    small modifs

--- Assets/ProjetPendu/Scripts/Pendu.cs
+++ Assets/ProjetPendu/Scripts/Pendu.cs
@@ -1,16 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Pendu : MonoBehaviour
-{
 
-    [SerializeField]
-    private string mot;
 
-    // Start is called before the first frame update
-    void Start()
+    public class Pendu : MonoBehaviour
     {
-        Debug.Log("Lancement pendu. Le mot à trouver est "+mot+".");
+
+        [SerializeField]
+        private string word;
+
+        int counter;
+
+        string chars;
+
+        public InputField input;
+        public Text ouput;
+
+
+        void Init(bool gameStart)
+        {
+
+            //    gameStart
+
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Lancement pendu. Le mot à trouver est " + word + ".");
+
+            ouput.text = "tapez une lettre";
+
+            Init(true);
+
+        }
+
+
+        public void onValidate()
+        {
+            Debug.Log("onValidate=" + input.text);
+
+            if (input.text.Length != 1) return;
+
+            Init(false);
+
+        }
     }
-}
